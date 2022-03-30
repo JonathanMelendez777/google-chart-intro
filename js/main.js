338669df0e88b61f47f04b4a0599e0016bfd7c13
Jonathan Melendez <34495781+JonathanMelendez777@users.noreply.github.com>
2022-03-29 18:06:23 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,37 @@
+// Load the Visualization API and the corechart package.
+google.charts.load('current', {'packages':['corechart']});
+
+// Set a callback to run when the Google Visualization API is loaded.
+google.charts.setOnLoadCallback(dibujarGrafica);
+
+
+
+// Callback that creates and populates a data table,
+// instantiates the pie chart, passes in the data and
+// draws it.
+function dibujarGrafica() {
+
+  // Create the data table.
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'Topping');
+  data.addColumn('number', 'Slices');
+  data.addRows([
+    ['Champiñones', 1],
+    ['Cebollas', 1],
+    ['Aceitunas', 1],
+    ['Queso', 3],
+    ['Pepperoni', 3],
+    ['Jamon', 3],
+    ['Piña', 2]
+  ]);
+
+  // Set chart options
+  var options = {'title':'¿Cuanta Pizza comí anoche?',
+                 'width':450,
+                 'height':300};
+
+  // Instantiate and draw our chart, passing in some options.
+  var chart = new google.visualization.PieChart(document.getElementById('grafica'));
+  chart.draw(data, options);
+}
+
